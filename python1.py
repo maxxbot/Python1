bb1bd7235e9a7d61fbdfc306c73ca7200373691b
maxxbot <brio0023@umn.edu>
2018-12-07 19:17:39 -0800

    updated python1.py

--- python1.py
+++ python1.py
@@ -1,3 +1,18 @@
 message = 'Hello World'
 
+message2 = """Jeb!
+is
+a
+MESS"""
+
 print('Hello World')
+print(message)
+print(message2)
+print(len(message2))
+print(message2[0])
+print(message2[0:4])
+print(message2[:4])
+
+#Print message all lower case
+print(message2.lower())
+
