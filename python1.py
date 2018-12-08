ed4e1b9ba26758cb1c1f086896ee4839eb030b65
maxxbot <brio0023@umn.edu>
2018-12-07 19:40:22 -0800

    Updated python1.py

--- python1.py
+++ python1.py
@@ -16,3 +16,33 @@ print(message2[:4])
 #Print message all lower case
 print(message2.lower())
 
+#Print message all upper case
+print(message2.upper())
+
+#Print times letter or phrase appears
+print(message2.count('e'))
+
+#Print index of letter or phrase
+print(message2.find('MESS'))
+
+#Find and replace method
+message3 = message2.replace('MESS','WASTE')
+print(message3)
+
+#String concatenate 
+
+greeting = 'Hello'
+name = 'Michael'
+print(greeting + ', ' + name)
+
+#Formatted string
+
+message = '{}, {}. Welcome!'.format(greeting, name)
+print(message)
+
+#F string
+
+message = f'{greeting}, {name}. Welcome!'
+print(message)
+
+
